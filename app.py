397d4436a9aa1de714acc6303c078f7c4897e104
Jennifer Carreno <jennifercarreno72@gmail.com>
2022-01-05 14:05:09 -0800

    users can upload using links

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 from flask import Flask, render_template, redirect, url_for, request
 from bson.objectid import ObjectId
 from pymongo import MongoClient
+from werkzeug.utils import secure_filename
 import os
+import item_functions
 
 host = os.environ.get('DB_URL')
 client = MongoClient(host=host)
 db = client.closet
 items = db.items
 app = Flask(__name__)
-
+UPLOAD_FOLDER = '/uploads'
 
 # home page
 @app.route('/')
@@ -26,11 +28,21 @@ def items_submit():
     item = {
         'name': request.form.get('name'),
         'item-photo': request.form.get('item-photo'),
-        'item-photo-link': request.form.get('item-photo-link')
+        'link': request.form.get('photo-link')
     }
     items.insert_one(item)
-    print(db.items)
+    # print(db.items)
+    print(item['name'])
+    print(item['link'])
+    # item_photo_func = item_functions.get_item_photo(items)
     return render_template('items.html', items=items.find())
+
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_file():
+   if request.method == 'POST':
+      f = request.files['file']
+      f.save(secure_filename(f.filename))
+      return 'file uploaded successfully'
 
 if __name__ == '__main__':
    app.run()
---
+++ item_functions.py
@@ -0,0 +1,9 @@
+import app 
+global item_photo_src 
+
+def get_item_photo(items):
+    # for item in items.find():
+    #     if item. != '':
+    #         item_photo_src = item['item-photo-link']
+    #         print(item['item-photo-link'])
+    return 

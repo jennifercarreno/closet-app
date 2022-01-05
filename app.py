f8ac7d2003a921ec272e4e25f3d88b2529a66c20
Jennifer Carreno <jennifercarreno72@gmail.com>
2022-01-04 20:56:10 -0800

    added basic home page and create new item page

--- app.py
+++ app.py
@@ -6,11 +6,30 @@ import os
 host = os.environ.get('DB_URL')
 client = MongoClient(host=host)
 db = client.closet
+items = db.items
 app = Flask(__name__)
 
+
+# home page
 @app.route('/')
 def index():
    return render_template('home.html')
+
+#prompts to create a new item 
+@app.route('/item/new')
+def item_new():
+    return render_template('item_new.html')
+
+# displays items
+@app.route('/items')
+def items_submit():
+    item = {
+        'name': request.form.get('name'),
+        'item-photo': request.form.get('item-photo'),
+        'item-photo-link': request.form.get('item-photo-link')
+    }
+    items.insert_one(item)
+    return render_template('items.html')
 
 if __name__ == '__main__':
    app.run()

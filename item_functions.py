025fab482f81503a5c0742340af4ffb081c6b32c
Jennifer Carreno <jennifercarreno72@gmail.com>
2022-01-07 18:43:43 -0800

    users can now filter by color, category, or both

--- item_functions.py
+++ item_functions.py
@@ -7,12 +7,29 @@ def filter(items):
     filtered_items = []
     filter_color = request.form.get('color')
     print(filter_color)
-    # filter_category = request.form.get('category')
+    filter_category = request.form.get('category')
 
-    for item in items.find():
-        item_color = item.get('color')
-        if item_color == filter_color:
-            filtered_items.append(item)
-            print(filtered_items)
+    # check to see if a filter has been selected
+    if filter_color != '--' and filter_category != '--':
+        # if only color filter is selected
+        if filter_color != '--' and filter_category == '--':
+            for item in items.find():
+                item_color = item.get('color')
+                if item_color == filter_color:
+                    filtered_items.append(item)
+        # if only category filter is selected
+        if filter_color == '--' and filter_category != '--':
+            for item in items.find():
+                item_category = item.get('category')
+                if item_category == filter_category:
+                    filtered_items.append(item)
+
+        if filter_color != '--' and filter_category != '--':
+            for item in items.find():
+                item_category = item.get('category')
+                item_color = item.get('color')
+                if item_category == filter_category and item_color == filter_color:
+                    filtered_items.append(item)
+
     return filtered_items
 
